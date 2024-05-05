947ded863c249e130a596fd0e13367fa9615c93e
koksao <roksanaklodka101@gmail.com>
2024-05-05 19:01:13 +0200

    create DAOs

--- src/main/java/com/devtiro/database/dao/impl/BookDaoImpl.java
+++ src/main/java/com/devtiro/database/dao/impl/BookDaoImpl.java
@@ -1,6 +1,7 @@
 package com.devtiro.database.dao.impl;
 
 import com.devtiro.database.dao.BookDao;
+import com.devtiro.database.domain.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BookDaoImpl implements BookDao {
@@ -9,5 +10,16 @@ public class BookDaoImpl implements BookDao {
 
     public BookDaoImpl(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Book book) {
+        jdbcTemplate.update(
+                "INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
+                book.getIsbn(),
+                book.getTitle(),
+                book.getAuthorId()
+        );
+
     }
 }
